046f39c03bf2dab8dbbdd323fc2ec03dde2ccc80
Vini1604 <vinicius.oliveira1998@hotmail.com>
2023-11-15 20:58:36 -0300

    Adicionado CORS

--- Program.cs
+++ Program.cs
@@ -90,6 +90,7 @@ if (app.Environment.IsDevelopment())
 app.UseHttpsRedirection();
 app.UseAuthentication();
 app.UseAuthorization();
+app.UseCors(options => {options.AllowAnyOrigin(); options.AllowAnyHeader(); options.AllowAnyMethod();});
 
 app.MapControllers();
 
