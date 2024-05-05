b37e1b3ade32a0ff8ec8234ffb3de7793f4b54fe
Vini1604 <vinicius.oliveira1998@hotmail.com>
2024-05-04 23:23:47 -0300

    Atualizado queries

--- Extensions/QueryExtensions.cs
+++ Extensions/QueryExtensions.cs
@@ -35,28 +35,32 @@ namespace AgendamentoWebAPI.Extensions
           WHERE ATC.medico_id = @idMedico;";
 
         public static string BuscarAgendamentosPaciente() => @"
-        SELECT A.id AS 'Id', A.medico_id AS 'MedicoId', VM.nome AS 'NomeMedico', A.paciente_id AS 'PacienteId', VP.nome AS 'NomePaciente', A.especialidade_id AS 'EspecialidadeId', E.descricao AS 'Especialidade', A.tipo_consulta_id AS 'TipoConsultaId', TC.descricao AS 'TipoConsulta', A.status_id AS 'StatusConsultaId', S.descricao AS 'StatusConsulta', A.data_agendada AS 'DataAgendamento' FROM agendamento A
+        SELECT A.id AS 'Id', A.medico_id AS 'MedicoId', VM.nome AS 'NomeMedico', A.paciente_id AS 'PacienteId', VP.nome AS 'NomePaciente', A.especialidade_id AS 'EspecialidadeId', E.descricao AS 'Especialidade', A.tipo_consulta_id AS 'TipoConsultaId', TC.descricao AS 'TipoConsulta', A.status_id AS 'StatusConsultaId', S.descricao AS 'StatusConsulta', A.data_agendada AS 'DataAgendamento', C.link_consulta As 'LinkConsulta' FROM agendamento A
             INNER JOIN v_medico VM ON VM.id = A.medico_id
             INNER JOIN especialidade E ON E.id = A.especialidade_id
             INNER JOIN v_paciente VP ON VP.IdPaciente = A.paciente_id
             INNER JOIN tipo_consulta TC ON TC.id = A.tipo_consulta_id
             INNER JOIN status S ON S.id = A.status_id
+            INNER JOIN consulta C ON A.id = C.agendamento_id
             WHERE VP.IdPaciente = @idPaciente
             ORDER BY A.data_agendada DESC;";
 
         public static string BuscarAgendamentosMedico() => @"
-        SELECT A.id AS 'Id', A.medico_id AS 'MedicoId', VM.nome AS 'NomeMedico', A.paciente_id AS 'PacienteId', VP.nome AS 'NomePaciente', A.especialidade_id AS 'EspecialidadeId', E.descricao AS 'Especialidade', A.tipo_consulta_id AS 'TipoConsultaId', TC.descricao AS 'TipoConsulta', A.status_id AS 'StatusConsultaId', S.descricao AS 'StatusConsulta', A.data_agendada AS 'DataAgendamento' FROM agendamento A
+        SELECT A.id AS 'Id', A.medico_id AS 'MedicoId', VM.nome AS 'NomeMedico', A.paciente_id AS 'PacienteId', VP.nome AS 'NomePaciente', A.especialidade_id AS 'EspecialidadeId', E.descricao AS 'Especialidade', A.tipo_consulta_id AS 'TipoConsultaId', TC.descricao AS 'TipoConsulta', A.status_id AS 'StatusConsultaId', S.descricao AS 'StatusConsulta', A.data_agendada AS 'DataAgendamento', C.link_consulta AS 'LinkConsulta' FROM agendamento A
             INNER JOIN v_medico VM ON VM.id = A.medico_id
             INNER JOIN especialidade E ON E.id = A.especialidade_id
             INNER JOIN v_paciente VP ON VP.IdPaciente = A.paciente_id
             INNER JOIN tipo_consulta TC ON TC.id = A.tipo_consulta_id
             INNER JOIN status S ON S.id = A.status_id
+            INNER JOIN consulta C on C.agendamento_id = A.id
             WHERE VM.id = @idMedico
             ORDER BY A.data_agendada DESC;";
 
         public static string InserirAgendamento() => @"
         INSERT INTO agendamento(medico_id, paciente_id, especialidade_id, tipo_consulta_id, status_id, data_agendada)
-        VALUES(@medicoId, @pacienteId, @especialidadeId, @tipoConsultaId, @statusId, @dataAgendada);";
+        VALUES(@medicoId, @pacienteId, @especialidadeId, @tipoConsultaId, @statusId, @dataAgendada);
+        SET @idAgendamento = LAST_INSERT_ID();
+        INSERT INTO consulta(agendamento_id, tipo_consulta_id, link_consulta) VALUES (@idAgendamento, 1, 'exame1.com.br');";
 
         public static string CancelarAgendamento() => @"
         UPDATE agendamento SET status_id = 3 WHERE status_id = 1 AND id = @agendamentoId;";
