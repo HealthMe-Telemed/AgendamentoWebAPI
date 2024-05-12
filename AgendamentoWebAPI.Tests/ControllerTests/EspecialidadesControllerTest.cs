fb62a7829dcf899cbc7a0946ca16c71d13a5f373
Vini1604 <vinicius.oliveira1998@hotmail.com>
2024-05-12 14:30:39 -0300

    Unit test especialidade controller

--- AgendamentoWebAPI.Tests/ControllerTests/EspecialidadesControllerTest.cs
+++ AgendamentoWebAPI.Tests/ControllerTests/EspecialidadesControllerTest.cs
@@ -2,6 +2,12 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using AgendamentoWebAPI.Models;
+using AgendamentoWebAPI.Services;
+using MedicoEspecialidadeWebAPI.Controllers;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Moq;
 using Xunit;
 
 namespace AgendamentoWebAPI.Tests.ControllerTests
@@ -9,9 +15,94 @@ namespace AgendamentoWebAPI.Tests.ControllerTests
     public class EspecialidadesControllerTest
     {
         [Fact]
-        public void BuscarEspecialidadesSemFiltroSucesso()
+        public async void BuscarEspecialidadesSemFiltroSucesso()
         {
-            Assert.True(true);
+            // Arrange
+            
+            var loggerMock = new Mock<ILogger<EspecialidadesController>>();
+            var especialidadesServiceMock = new Mock<IEspecialidadesService>();
+            int idMedico = 0;
+            List<Especialidade> especialidadesMock = new List<Especialidade>(){
+                new Especialidade(){ Id = 1, NomeEspecialidade = "Psicologia"},
+                new Especialidade(){ Id = 2, NomeEspecialidade = "Psiquiatria"}
+            };
+            especialidadesServiceMock.Setup(s => s.EncontrarEspecialidades()).ReturnsAsync(especialidadesMock);
+            var controller = new EspecialidadesController(loggerMock.Object, especialidadesServiceMock.Object);
+            
+            // Act
+
+            var result = await controller.GetEspecialidades(idMedico);
+
+            // Assert
+
+            Assert.IsType<OkObjectResult>(result);
         }
+
+        [Fact]
+        public async void BuscarEspecialidadesComFiltroSucesso()
+        {
+            // Arrange
+            
+            var loggerMock = new Mock<ILogger<EspecialidadesController>>();
+            var especialidadesServiceMock = new Mock<IEspecialidadesService>();
+            int idMedico = 1;
+            List<Especialidade> especialidadesMock = new List<Especialidade>(){
+                new Especialidade(){ Id = 1, NomeEspecialidade = "Psicologia"},
+                new Especialidade(){ Id = 2, NomeEspecialidade = "Psiquiatria"}
+            };
+            especialidadesServiceMock.Setup(s => s.EncontrarEspecialidadesPorMedico(idMedico)).ReturnsAsync(especialidadesMock);
+            var controller = new EspecialidadesController(loggerMock.Object, especialidadesServiceMock.Object);
+            
+            // Act
+
+            var result = await controller.GetEspecialidades(idMedico);
+
+            // Assert
+
+            Assert.IsType<OkObjectResult>(result);
+        }
+
+        [Fact]
+        public async void BuscarEspecialidadesSemFiltroListaVazia()
+        {
+            // Arrange
+            
+            var loggerMock = new Mock<ILogger<EspecialidadesController>>();
+            var especialidadesServiceMock = new Mock<IEspecialidadesService>();
+            int idMedico = 0;
+            List<Especialidade> especialidadesMock = new List<Especialidade>();
+            especialidadesServiceMock.Setup(s => s.EncontrarEspecialidades()).ReturnsAsync(especialidadesMock);
+            var controller = new EspecialidadesController(loggerMock.Object, especialidadesServiceMock.Object);
+            
+            // Act
+
+            var result = await controller.GetEspecialidades(idMedico);
+
+            // Assert
+
+            Assert.IsType<NoContentResult>(result);
+        }
+
+        [Fact]
+        public async void BuscarEspecialidadesComFiltroListaVazia()
+        {
+            // Arrange
+            
+            var loggerMock = new Mock<ILogger<EspecialidadesController>>();
+            var especialidadesServiceMock = new Mock<IEspecialidadesService>();
+            int idMedico = 1;
+            List<Especialidade> especialidadesMock = new List<Especialidade>();
+            especialidadesServiceMock.Setup(s => s.EncontrarEspecialidadesPorMedico(idMedico)).ReturnsAsync(especialidadesMock);
+            var controller = new EspecialidadesController(loggerMock.Object, especialidadesServiceMock.Object);
+            
+            // Act
+
+            var result = await controller.GetEspecialidades(idMedico);
+
+            // Assert
+
+            Assert.IsType<NoContentResult>(result);
+        }
+
     }
 }
